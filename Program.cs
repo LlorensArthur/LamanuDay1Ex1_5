ad1e0c9a40f14858273f3200edae22feb27fc35c
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:04:38 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string Name = "";
+string Surname = "";
+Console.WriteLine("Veuillez entrer votre nom");
+Name = Console.ReadLine();
+Console.WriteLine("Veuillez entre votre nom");
+Surname = Console.ReadLine();
+Console.WriteLine($"Bonjour {Name} {Surname}, nous sommes le {DateTime.Now.ToLongDateString()}, comment allez-vous?");
